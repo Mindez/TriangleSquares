9f595b19a8d73c54da721655013c8af807dd7b7a
Mindez <mindez@gmail.com>
2016-07-14 23:27:05 +0100

    implemented big-numbers. this kills the performance.

--- tests/tests.js
+++ tests/tests.js
@@ -1,63 +1,58 @@
 'use strict';
 
-var chai   = require('chai'),
-    expect = chai.expect,
-    trisqy = require('../trisqy');
+var BigNumber = require('big-number'),
+    chai      = require('chai'),
+    expect    = chai.expect,
+    trisqy    = require('../trisqy');
 
 describe('Basic Maths Tests', function() {
     it('should correctly calculate the first 5 triangle numbers', function() {
         var result = [];
         for (var i = 0; i < 5; i++) {
-            result.push(trisqy._getTriangle(i+1));
+            result.push(trisqy._getTriangle(BigNumber(i+1)).toString());
         }
-        expect(result).to.eql([1, 3, 6, 10, 15]);
+        expect(result).to.eql(['1', '3', '6', '10', '15']);
     });
     
     it('should correctly calculate the first 5 square numbers', function() {
         var result = [];
         for (var i = 0; i < 5; i++) {
-            result.push(trisqy._getSquare(i+1));
+            result.push(trisqy._getSquare(BigNumber(i+1)).toString());
         }
-        expect(result).to.eql([1, 4, 9, 16, 25]);
+        expect(result).to.eql(['1', '4', '9', '16', '25']);
     });
 
     it('should return 0 for the zeroth triangle number', function() {
-        var result = trisqy._getTriangle(0);
-        expect(result).to.eql(0);
+        var result = trisqy._getTriangle(BigNumber(0));
+        expect(result).to.eql(BigNumber(0));
     });
 
     it('should return 0 for the zeroth square number', function() {
-        var result = trisqy._getSquare(0);
-        expect(result).to.eql(0);
+        var result = trisqy._getSquare(BigNumber(0));
+        expect(result).to.eql(BigNumber(0));
     });
 });
 
 describe('Increment Tests', function() {
     it('should increment triangular numbers correctly', function() {
         var triangle = {
-            before : 1,
-            after  : trisqy._getTriangle(1),
+            before : BigNumber(1),
+            after  : trisqy._getTriangle(BigNumber(1)),
             func   : trisqy._getTriangle
         };
         trisqy._increment(triangle);
-        expect(triangle).to.be.eql({
-            before : 2,
-            after  : 3,
-            func   : trisqy._getTriangle
-        });
+        expect(triangle.before.toString()).to.be.eql('2');
+        expect(triangle.after.toString()).to.be.eql('3');
     });
 
     it('should increment square numbers correctly', function() {
         var square = {
-            before : 1,
-            after  : trisqy._getSquare(1),
+            before : BigNumber(1),
+            after  : trisqy._getSquare(BigNumber(1)),
             func   : trisqy._getSquare
         };
         trisqy._increment(square);
-        expect(square).to.be.eql({
-            before : 2,
-            after  : 4,
-            func   : trisqy._getSquare
-        });
+        expect(square.before.toString()).to.be.eql('2');
+        expect(square.after.toString()).to.be.eql('4');
     });
 });
--- trisqy.js
+++ trisqy.js
@@ -1,43 +1,39 @@
 'use strict';
 
-var maxInt = Number.MAX_SAFE_INTEGER;
+var BigNumber = require('big-number');
 
 var trisqy = {
     start : function(options) {
         var startSquare   = options.startSquare   || 1,
             startTriangle = options.startTriangle || 1,
-            toReturn      = options.toReturn      || maxInt;
+            toReturn      = options.toReturn      || -1;
 
         // initialise the square and triangle objects
         var finished = false,
             numMatches = 0,
             square = {
-                before : startSquare,
-                after  : this._getSquare(startSquare),
+                before : BigNumber(startSquare),
+                after  : this._getSquare(BigNumber(startSquare)),
                 func   : this._getSquare
             },
             triangle = {
-                before : startTriangle,
-                after  : this._getTriangle(startTriangle),
+                before : BigNumber(startTriangle),
+                after  : this._getTriangle(BigNumber(startTriangle)),
                 func   : this._getTriangle
             };
 
         while (!finished) {
-            if (square.after === triangle.after) {
-                if (square.after <= maxInt) {
-                    this._output({
-                        triangle : triangle,
-                        square   : square
-                    });
-                    numMatches++;
-                    if (numMatches >= toReturn) {
-                        finished = true;
-                    }
-                } else {
+            if (square.after.equals(triangle.after)) {
+                this._output({
+                    triangle : triangle,
+                    square   : square
+                });
+                numMatches++;
+                if (numMatches === toReturn) {
                     finished = true;
                 }
             }
-            if (square.after > triangle.after) {
+            if (square.after.gt(triangle.after)) {
                 this._increment(triangle);
             } else {
                 this._increment(square);
@@ -46,20 +42,25 @@ var trisqy = {
     },
 
     _getTriangle : function(x) {
-        return ((x * (x+1)) / 2);
+        var orig = BigNumber(x.toString());
+        return (x.multiply(x).add(orig).divide(2));
     },
 
     _getSquare : function(x) {
-        return (x * x);
+        return x.multiply(x);
     },
 
     _increment : function(obj) {
-        obj.before++;
-        obj.after = obj.func(obj.before);
+        obj.before.plus(1);
+        var after = BigNumber(obj.before.toString());
+        obj.after = obj.func(after);
     },
 
     _output : function(obj) {
-        console.log('Match found! ' + obj.triangle.after + ' = tri(' + obj.triangle.before + ') = sq(' + obj.square.before + ') = ' + obj.square.after);
+        console.log('Match found! ' + obj.triangle.after.toString() +
+            ' = tri(' + obj.triangle.before.toString() + ')' +
+            ' = sqr(' + obj.square.before.toString() + ')' +
+            ' = ' + obj.square.after.toString());
     }
 };
 
